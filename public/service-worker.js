b54028f684a3ea8e31013f0e2114ad104f5ff048
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-04-10 09:50:06 -0400

    Fixed errors in service worker, created db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,76 +2,69 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/favicon.ico",
   "/manifest.webmanifest",
-  "/assets/css/style.css",
-  "/assets/js/loadImages.js",
-  "/assets/images/icon-192x192.png",
-  "/assets/images/icon-512x512.png",
+  "/styles.css",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 // Install service worker
-self.addEventListener("install", function(event) {
-    // Pre-cache image data
-    event.waitUnit(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    );
-
-    // Pre-cache all static assets
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-
-    // Tell browser to activate once install is finished
-    self.skipWaiting();
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
 // Activate service worker and remove old data from cache
-self.addEventListener("activate", function(event) {
-    event.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
         })
-    );
-})
+      );
+    })
+  );
+});
 
 // Enable service worker to intercept network requests
-self.addEventListener('fetch', function(event) {
-    if (event.request.url.includes('/api/')) {
-        console.log('[Service Worker] Fetch (data)', event.request.url);
-        
-        event.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(event.request)
-                .then(response => {
-                    if (response.status === 200) {
-                        cache.put(event.request.url, response.clone());
-                    }
-
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(event.request);
-                });
-            })
-        );
-
-        return;
-    }
-
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
     event.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(event.request).then(response => {
-                return response || fetch(event.request);
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(event.request);
             });
         })
+        .catch((err) => console.log(err))
     );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
 });
